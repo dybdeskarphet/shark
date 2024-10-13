6420baa06dda39f4ee08a9a639b75b02084476f5
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-10-13 20:50:13 +0300

    feat(main): Implement a prompt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+use std::io::Write;
+
+fn execute(input: String) {
+    let input_str = input.as_str();
+    match input_str.trim() {
+        "exit" => std::process::exit(0),
+        "help" => {
+            println!("Hello, this is Shark, a custom shell without a purpose.");
+        }
+        _ => print!("{}", input),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect(".");
+        execute(input);
+    }
 }
