85bd0c8988b24db7fb61b5babe4fc2b9547c7c8d
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-10-14 17:19:45 +0300

    feat(main): Implement basic command execution

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
 use std::io;
 use std::io::Write;
+use std::process::Command;
+
+fn command_parse_execute(command: String) {
+    let command_output = if command.trim().contains(char::is_whitespace) {
+        let (program, args) = command
+            .split_once(char::is_whitespace)
+            .expect("Something went wrong while parsing the command");
+        let mut args: Vec<&str> = args.split(char::is_whitespace).collect();
+
+        if let Some(element) = args.last() {
+            if *element == "" {
+                args.pop();
+            }
+        }
+
+        Command::new(program).args(&args).status()
+    } else {
+        Command::new(command.trim()).status()
+    };
+
+    match &command_output {
+        Ok(_) => (),
+        Err(e) => println!("{}", e),
+    }
+}
 
 fn execute(input: String) {
     let input_str = input.as_str();
@@ -8,7 +33,7 @@ fn execute(input: String) {
         "help" => {
             println!("Hello, this is Shark, a custom shell without a purpose.");
         }
-        _ => print!("{}", input),
+        _ => command_parse_execute(input),
     }
 }
 
