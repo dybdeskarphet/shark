54e17e3d5747e4407ea05613fbd1aa1ad3764011
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2024-10-19 03:46:05 +0300

    refactor(shell_command): use spawn instead of status for future implementations

--- src/shell_command.rs
+++ src/shell_command.rs
@@ -1,5 +1,8 @@
 use crate::builtin_commands::run_builtin;
 use crate::input_parser::parse;
+use nix::sys::signal::kill;
+use nix::sys::signal::SIGINT;
+use nix::unistd::Pid;
 use std::process::Command;
 
 pub struct ShellCommand {
@@ -14,20 +17,29 @@ impl ShellCommand {
     }
 
     fn external_command(&self) {
-        let output;
+        let child;
+
         match &self.args {
             Some(args) => {
                 let args_ref: Vec<&str> = args.iter().map(|s| s.as_str()).collect();
-                output = Command::new(&self.command).args(args_ref).status();
+                child = Command::new(&self.command).args(args_ref).spawn();
             }
             None => {
-                output = Command::new(&self.command).status();
+                child = Command::new(&self.command).spawn();
             }
         }
 
-        match output {
-            Ok(_) => (),
-            Err(e) => println!("shark: {}", e),
+        match child {
+            Ok(child_process) => {
+                let result = child_process.wait_with_output();
+                match result {
+                    Ok(_) => (),
+                    Err(e) => print!("shark: couldn't wait for the process: {}", e),
+                }
+            }
+            Err(e) => {
+                println!("shark: cannot run the command: {}", e);
+            }
         }
     }
 
